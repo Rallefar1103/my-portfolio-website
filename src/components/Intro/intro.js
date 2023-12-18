6d002c950b5ae19ee3f4818cac47ee5fa8a971f8
Rallefar1103 <rasmus@turftank.com>
2023-12-18 15:03:35 -0800

    customizes the intro section

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import './intro.css';
-import bg from '../../assets/image.png';
-import btnImg from '../../assets/hireme.png';
-import { Link } from 'react-scroll';
-
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/profile.jpeg";
+import linkedinIcon from "../../assets/linkedin.png";
+import githubIcon from "../../assets/github.png";
+import emailIcon from "../../assets/gmail.png";
 const Intro = () => {
-    return (
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Smith</span> <br />Website Designer</span>
-                <p className="introPara">I am a skilled web designer with experience in creating<br />visually appealing and user friendly websites.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> Hire Me</button></Link>
-            </div>
-            <img src={bg} alt="Profile" className="bg" />
-        </section>
-    );
-}
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <div className="bg-container">
+          <img src={bg} alt="Profile" className="bg" />
+        </div>
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Rasmus</span> <br />
+          Full-Stack Software Engineer <br />
+          <p className="subtitle">
+            with five years of professional industry experience including two
+            years as a tech-startup founder.
+          </p>
+        </span>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/rasmus-henriksen-a5b660109/"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinIcon} alt="Linkedin" className="link" />
+          </a>
+          <a href="mailto:rasmus.henriksen@live.dk" rel="noopener noreferrer">
+            <img src={emailIcon} alt="Email" className="link" />
+          </a>
+
+          <a
+            href="https://github.com/Rallefar1103?tab=repositories"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="Github" className="link" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
