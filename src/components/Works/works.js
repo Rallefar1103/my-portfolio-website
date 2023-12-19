d61179b6ded58416515bbe88d631ce2ae7b8c504
Rallefar1103 <rasmus@turftank.com>
2023-12-18 20:38:01 -0800

    adds titles and aligns portfolio projects in a block section

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -12,17 +12,33 @@ const Works = () => {
     <section id="works">
       <h2 className="worksTitle">My Portfolio</h2>
       <span className="worksDesc">
-        I take pride in paying attention to the smallest details. In my work
-        nothing is left to chance and I only deliver code of the highest
-        quality.
+        Below you will find a selection of my web and mobile applications.
       </span>
       <div className="worksImgs">
-        <img src={travelPlanner} alt="" className="worksImg" />
-        <img src={foodtruck} alt="" className="worksImg" />
-        <img src={foodieFriends} alt="" className="worksImg" />
-        <img src={washee} alt="" className="worksImg" />
-        <img src={delivery} alt="" className="worksImg" />
-        <img src={dataAnalytics} alt="" className="worksImg" />
+        <div className="imgContainer">
+          <span className="imgTitle">Intelligent Travel Assistant</span>
+          <img src={travelPlanner} alt="" className="worksImg" />
+        </div>
+        <div className="imgContainer">
+          <span className="imgTitle">Food Truck Tracker</span>
+          <img src={foodtruck} alt="" className="worksImg" />
+        </div>
+        <div className="imgContainer">
+          <span className="imgTitle">Foodie Friends</span>
+          <img src={foodieFriends} alt="" className="worksImg" />
+        </div>
+        <div className="imgContainer">
+          <span className="imgTitle">Eco Laundry Assistant</span>
+          <img src={washee} alt="" className="worksImg" />
+        </div>
+        <div className="imgContainer">
+          <span className="imgTitle">Food Delivery Application</span>
+          <img src={delivery} alt="" className="worksImg" />
+        </div>
+        <div className="imgContainer">
+          <span className="imgTitle">Data Analytics Microservice</span>
+          <img src={dataAnalytics} alt="" className="worksImg" />
+        </div>
       </div>
     </section>
   );
