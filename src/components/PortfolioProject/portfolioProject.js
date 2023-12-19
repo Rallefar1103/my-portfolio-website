430a04b002f04c2332e9c8bb9c8b6fcb2428890a
Rallefar1103 <rasmus@turftank.com>
2023-12-18 23:26:24 -0800

    links the project id to the page

--- src/components/PortfolioProject/portfolioProject.js
+++ src/components/PortfolioProject/portfolioProject.js
@@ -1,15 +1,19 @@
 import { useParams } from "react-router-dom";
+import allProjects from "../../data/allProjects";
 
 const PortfolioProject = () => {
   let { projectId } = useParams(); // This will match the :projectId in your route
 
-  // Logic to fetch or determine the project details based on the projectId
+  const project = allProjects.find((p) => p.id === projectId);
+
+  if (!project) {
+    return <div> 404 Project not found </div>;
+  }
 
   return (
     <div>
-      {/* Render your project details here */}
-      <h1>Project: {projectId}</h1>
-      {/* ... */}
+      <h1 className="project-name">{project.title}</h1>
+      <p className="project-description">{project.description}</p>
     </div>
   );
 };
--- src/data/allProjects.js
+++ src/data/allProjects.js
@@ -1,4 +1,42 @@
 import "./project";
 import Project from "./project";
 
-let allProjects = [new Project(), new Project(), new Project(), new Project()];
+let allProjects = [
+  new Project(
+    "travel-planner",
+    "Smart Travel Assistant",
+    "Some description goes here.."
+  ),
+  new Project(
+    "food-truck-tracker",
+    "Food Truck Tracker",
+    "Some description goes here.."
+  ),
+  new Project(
+    "foodie-friends",
+    "Foodie Friends",
+    "Some description goes here.."
+  ),
+  new Project(
+    "washee",
+    "Eco Laundry Assistant",
+    "Some description goes here.."
+  ),
+  new Project(
+    "food-delivery",
+    "Food Delivery Application",
+    "Some description goes here.."
+  ),
+  new Project(
+    "data-analytics",
+    "Data Analytics Microservice",
+    "Some description goes here.."
+  ),
+  new Project(
+    "shopday",
+    "Local Rewards Shopping App",
+    "Some description goes here.."
+  ),
+];
+
+export default allProjects;
