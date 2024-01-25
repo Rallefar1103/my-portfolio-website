5e8cd5b4e28eca84a81be2695741e7dd7c9d5850
Rallefar1103 <rasmus@turftank.com>
2024-01-24 16:29:48 -0800

    makes the navbar responsive and adds mobile layout

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { Link as ScrollLink } from "react-scroll";
 import GmailIcon from "../../assets/gmail.png";
+import MenuIcon from "../../assets/menu.png";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   const NavLink = ({ to, children }) => {
     return (
       <ScrollLink
@@ -24,10 +31,24 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="desktopMenu">
         <NavLink to="intro">Home</NavLink>
-        <NavLink to="skills">About</NavLink>
+        <NavLink to="skills">Skills</NavLink>
         <NavLink to="works">Portfolio</NavLink>
         <NavLink to="articles">Articles</NavLink>
       </div>
+      <button className="hamburger-btn" onClick={toggleMobileMenu}>
+        <img src={MenuIcon} alt="Menu" className="menu-icon" />
+      </button>
+      {isMobileMenuOpen && (
+        <div className="mobileMenu">
+          <button className="closeMenu" onClick={toggleMobileMenu}>
+            X
+          </button>
+          <NavLink to="intro">Home</NavLink>
+          <NavLink to="skills">Skills</NavLink>
+          <NavLink to="works">Portfolio</NavLink>
+          <NavLink to="articles">Articles</NavLink>
+        </div>
+      )}
       <a href="mailto:rasmus.henriksen@live.dk" rel="noopener noreferrer">
         <div className="email-btn-wrapper">
           <button className="email-btn">
@@ -36,8 +57,6 @@ const Navbar = () => {
           <p>Contact Me</p>
         </div>
       </a>
-
-      {/* ... other menu items and mobile menu ... */}
     </nav>
   );
 };
